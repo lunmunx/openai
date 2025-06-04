6a71d622610c1273484c1375b76bafdbf2e47fc1
lunmunx <lunmunx@proton.me>
2025-06-04 23:57:35 +0200

    Add TypeScript aggregator for Everli ingestion

---
+++ packages/ui/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3001, () => {
+  console.log('UI server running on http://localhost:3001');
+});
